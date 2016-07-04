965dbb3aee868022f49d3d2f61c32284e80cc489
Josh English <josh@joshenglish.com>
2016-07-04 14:57:49 -0500

    Adding the main.jsx and index.html

--- source/main.jsx
+++ source/main.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, browserHistory } from 'react-router'
 
-render(<h1>Hello</h1>, document.getElementById('react-render'))
+import routes from './imports/routes.jsx'
+
+render(<Router routes={routes} history={browserHistory} />, document.getElementById('react-render'))
